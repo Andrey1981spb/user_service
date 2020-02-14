778b968c3493d8c640cff9d71018b622f4ff7e27
andrej <andrey1981spb@gmail.com>
2020-02-14 12:14:53 +0300

    deploy phone valid2

--- src/main/java/ru/spb/dreamwhite/repository/country/CountryRepository.java
+++ src/main/java/ru/spb/dreamwhite/repository/country/CountryRepository.java
@@ -1,13 +1,11 @@
 package ru.spb.dreamwhite.repository.country;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Repository;
 import ru.spb.dreamwhite.model.Country;
 import ru.spb.dreamwhite.repository.CrudUserRepository;
 
 import java.util.List;
-
 
 @Repository
 public class CountryRepository {
