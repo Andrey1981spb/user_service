42808fb8706ec79f0b9b7e0028b284cb25a595bc
Andrey1981spb <andrey1981spb@gmail.com>
2020-03-03 09:37:43 +0300

    encoder to controller add

---
+++ src/main/java/ru/spb/dreamwhite/web/HtmlTestingController.java
@@ -0,0 +1,18 @@
+package ru.spb.dreamwhite.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+//@RequestMapping ( "/" )
+public class HtmlTestingController {
+
+    @GetMapping
+    public String showPage() {
+        return "redirect:access_denied";
+    }
+
+}
+
+
